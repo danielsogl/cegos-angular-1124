6a54f303d84086c0bb197b8b1f9e5b8ce96799af
Daniel Sogl <daniel.sogl@thinktecture.com>
2024-11-25 14:52:14 +0100

    add cfs

--- src/app/home/blog-list/blog-list.component.ts
+++ src/app/home/blog-list/blog-list.component.ts
@@ -1,14 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Entry } from '../entry';
-import { HomeComponent } from '../home.component';
+import { BlogEntryComponent } from './blog-entry/blog-entry.component';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-blog-list',
   standalone: true,
-  imports: [],
+  imports: [BlogEntryComponent, NgFor, NgIf],
   templateUrl: './blog-list.component.html',
   styleUrl: './blog-list.component.css',
 })
 export class BlogListComponent {
-  @Input() blogentries: Entry[] = [];
+  @Input({ required: true }) blogentries: Entry[] = [];
 }
